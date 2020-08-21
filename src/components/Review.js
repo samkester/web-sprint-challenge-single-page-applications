8d45afb32de370f4d1de75af36f069a33b4d3253
Sam Kester <samkester@gmail.com>
2020-08-21 10:44:17 -0600

    filled out Review.js

--- src/components/Review.js
+++ src/components/Review.js
@@ -2,12 +2,18 @@ import React from "react";
 import {Link} from "react-router-dom";
 import ReviewOrder from "./ReviewOrder";
 
-const Review = () => {
+const Review = ({orders}) => {
     return(
         <div>
             <h2>Review Your Orders</h2>
+            {orders && orders.map(order => 
+                <ReviewOrder key="order.id" order={order} />
+            )}
+            {orders.count > 0 ? 
             <Link to="/order">Add another order</Link>
-            <ReviewOrder />
+            :
+            <div>You don't have any orders yet. Why not <Link to="/order">create one?</Link></div>
+            }
         </div>
     );
 };
--- src/components/ReviewOrder.js
+++ src/components/ReviewOrder.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-const ReviewOrder = () => {
+const ReviewOrder = ({order}) => {
+    const toppingsFromOrder = () => {
+        const result = [];
+        for(let i in order)
+        {
+            if(i.includes("topping"))
+            {
+                result.push(i.split("_")[1]);
+            }
+        }
+        return result.join();
+    }
+
     return(
         <div>
-            <h3>I am a pizza.</h3>
+            <h3>Order No. {order.id}</h3>
+            <p>Name: {order.name}</p>
+            <p>Size: {order.size}</p>
+            <p>Toppings: {toppingsFromOrder()}</p>
+            <p>Instructions: {order.instructions}</p>
         </div>
     );
 };
