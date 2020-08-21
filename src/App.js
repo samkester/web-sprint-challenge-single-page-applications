a7de46f366ad50ff77d38e2237915c7156e20d1c
Sam Kester <samkester@gmail.com>
2020-08-21 09:59:54 -0600

    setup data.js; setup Axios call

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {Switch, Link, NavLink, Route} from "react-router-dom";
 import Home from "./components/Home";
 import Review from "./components/Review";
 import Order from "./components/Order";
+import Axios from "axios";
+import data from "./data/data";
 
 const defaultFormValues = {
   name: "",
   size: "",
-  toppings: {},
   instructions: "",
 };
 
@@ -20,6 +21,21 @@ const App = () => {
   const [formValues, setFormValues] = useState(defaultFormValues);
   const [formErrors, setFormErrors] = useState(defaultFormErrors);
   const [orders, setOrders] = useState([]);
+
+  const setFormValue = (name, value) => {
+    setFormValues({...formValues, [name]: value});
+  };
+
+  const resetOrder = () => {
+    setFormValues(defaultFormValues);
+  }
+
+  const submitOrder = () => {
+    Axios.post(data.pizzaAPI, formValues)
+    .then(response => setOrders(orders.concat(response.data)))
+    .catch(error => console.log(error))
+    .finally(resetOrder());
+  };
   
   return (
     <>
@@ -34,7 +50,7 @@ const App = () => {
           <Review orders={orders} />
         </Route>
         <Route path="/order">
-          <Order values={formValues} errors={formErrors} />
+          <Order values={formValues} errors={formErrors} setValue={setFormValue} reset={resetOrder} submit={submitOrder} />
         </Route>
         <Route path="/">
           <Home />
